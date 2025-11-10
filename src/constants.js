98d4c1913c19dbff80431799e8a31814acfdc35c
Faisalhere7 <syedfaisalrazaofficial7@gmail.com>
2025-11-10 17:24:22 +0500

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,254 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/fiverr.png';
+import agcLogo from './assets/company_logo/fiverr.png';
+import newtonschoolLogo from './assets/company_logo/fiverr.png';
+
+// Education Section Logo's
+import pny from './assets/education_logo/pny.png';
+import uos from './assets/education_logo/uos.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      // { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      // { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      // { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: "MERN Stack Developer",
+      company: "Freelacing",
+      date: "June 2024 - Present",
+      desc: " Developed several personal and academic web projects using React.js, Node.js, Express.js, and MongoDB.Built responsive and user-friendly interfaces with HTML, CSS, JavaScript, and modern frameworks.Focused on creating scalable, performance-optimized applications with clean and maintainable code.Gained hands-on experience in full-stack development, RESTful APIs, and project deployment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        " Next Js",
+      ],
+    },
+    {
+      id: 1,
+      img: agcLogo,
+      role: "Digital Marketing",
+      company: "Indepedent",
+      date: "Oct 2021 - March 2024",
+      desc: "Managed end-to-end digital marketing projects, including content strategy, SEO, and targeted advertising campaigns.Planned and executed social media campaigns to increase engagement and brand visibility.Analyzed campaign performance and optimized strategies for better ROI and audience reach.Built and maintained strong client relationships through regular updates, reporting, and timely delivery of marketing results.",
+      skills: [
+        "Social Media Marketing",
+        "Compaigns",
+        "Accounts Management",
+        "SEO",
+        "Accounts Growth",
+        ,
+      ],
+    },
+    {
+      id: 2,
+      img: newtonschoolLogo,
+      role: "Graphic Design",
+      company: "Indepedent",
+      date: "September 2021 - August 2022",
+      desc: "Designed creative visuals, logos, posters, and branding assets tailored to client requirements.Collaborated with diverse clients to bring their ideas to life through visually appealing and professional designs.Ensured high-quality deliverables while maintaining brand consistency across all design materials.Developed a strong understanding of design principles, typography, and color theory to create impactful visuals.",
+      skills: [
+        "Canva",
+        "Calligraphy",
+        "Logo",
+        "Posters",
+        "UI",
+        "Branding Assets",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: uos,
+      school: "University of Sargodha ",
+      date: "Sept 2021 - June 2025",
+      grade: "3.4/4 CGPA",
+      desc: "I completed my BS degree in Information Technology (16-year education) from the University of Sargodha. The program provided me with a strong foundation in programming, software development, networking, databases, and core IT principles. I studied courses such as Data Structures, Object-Oriented Programming, Operating Systems, Database Systems, Networking, Cloud Computing, and Information Security. I also participated in academic activities and project work that strengthened my practical skills. My experience at the University of Sargodha played an important role in shaping my technical abilities and overall professional growth.",
+      degree: "Infomation Technology",
+    },
+    {
+      id: 1,
+      img: pny,
+      school: "PNY Trainings Arfa Tower Lahore",
+      date: "June 2024 - November 2024",
+      grade: "73.2%",
+      desc: "I completed my training in MERN Stack Development from PNY Trainings at Arfa Tower, Lahore. The program helped me build strong practical skills in modern web development using MongoDB, Express.js, React.js, and Node.js. I worked on hands-on projects covering front-end and back-end development, API integration, database management, and application deployment. The training sharpened my ability to create efficient, scalable applications and strengthened my understanding of full-stack development practices.",
+      Certificate: "MERN Stack Development",
+    },
+ 
+  ];
+  
+  export const projects = [
+
+    {
+      id: 2,
+      title: "Movie Recommendation App",
+      description:
+        "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+      image: movierecLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/",
+      webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+   
+    {
+      id: 4,
+      title: "Task Reminder Chrome Extension Tool",
+      description:
+        "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+      image: taskremLogo,
+      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+      github: "https://github.com/",
+      webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    },
+    {
+      id: 5,
+      title: "HMS (FYP)",
+      description:
+        "The Hospital Management System (HMS) is designed for Any Hospital to replace their existing manual, paper based system. The new system is to control the following information; patient information, room availability, staff and operating room schedules, and patient invoices. These services are to be provided in an efficient, cost effective manner, with the goal of reducing the time and resources currently required for such tasks. A significant part of the operation of any hospital involves the acquisition, management and timely retrieval of great volumes of information. This information typically involves; patient personal information and medical history, staff information, room and ward scheduling, staff scheduling, operating theater scheduling and various facilities waiting lists.",
+      image: webverLogo,
+      tags: ["HTML", "Tailwind CSS", "JavaScript","React js","Node js","Espress js","Mongodb","Restful API's", "Shadecn UI"],
+      github: "https://github.com/",
+      webapp: "https://google.com/",
+    },
+    {
+      id: 6,
+      title: "Age Calculator",
+      description:
+        "A simple JavaScript app that calculates a user’s exact age based on their date of birth. It takes input, processes the difference between the current date and the birth date, and displays the age in years, months, and days.",
+      image: cmLogo,
+      tags: ["HTML", "CSS", "Java Script"],
+      github: "https://",
+      webapp: "http",
+    },
+    {
+      id: 7,
+      title: "Image Search App",
+      description:
+        "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+      image: imagesearchLogo,
+      tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+      github: "https://github.com/",
+      webapp: "https://imagsearch.netlify.app/",
+    },
+    {
+      id: 8,
+      title: "Image Background Remover",
+      description:
+        "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+      image: removebgLogo,
+      tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+      github: "https://github.com/",
+      webapp: "https://removeyourbg.netlify.app/",
+    },
+  ];  
